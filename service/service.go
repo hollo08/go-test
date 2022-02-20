a61f929a7212c61f7db31f10dfcb9363d73efc66
shitou <hollo08@126.com>
2022-02-20 23:19:46 +0800

    cert

--- service/service.go
+++ service/service.go
@@ -1,15 +1,21 @@
 package main
-
+//https://www.cnblogs.com/baoshu/p/13507260.html#head4
 import (
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 	"test/service/product"
 )
 
 func main() {
+	tls, err := credentials.NewServerTLSFromFile("service/cert/server.crt", "service/cert/server_no_password.key")
+	if err != nil {
+		log.Fatal("服务端获取证书失败: ", err)
+	}
+
 	// 1. new一个grpc的server
-	rpcServer := grpc.NewServer()
+	rpcServer := grpc.NewServer(grpc.Creds(tls))
 
 	// 2. 将刚刚我们新建的ProdService注册进去
 	product.RegisterProdServiceServer(rpcServer, new(product.ProdService))
