c48d0a175eb552d847661ebe1499ac4b413aec26
shitou <shitou@bolo.em>
2022-02-23 18:46:10 +0800

    a

--- server/token/TokenService.go
+++ server/token/TokenService.go
@@ -1,88 +1,30 @@
 package token
 
 import (
-	"context"
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
-	"google.golang.org/grpc/metadata"
-	"time"
+	"golang.org/x/net/context"
 )
 
-func CreateToken(userName string) (tokenString string) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"iss":      "lora-app-server",
-		"aud":      "lora-app-server",
-		"nbf":      time.Now().Unix(),
-		"exp":      time.Now().Add(time.Hour).Unix(),
-		"sub":      "user",
-		"username": userName,
-	})
-	tokenString, err := token.SignedString([]byte("verysecret"))
-	if err != nil {
-		panic(err)
-	}
-	return tokenString
+// Server represents the gRPC server
+type Server struct {
 }
 
-// AuthToken 自定义认证
-type AuthToken struct {
-	Token string
-}
+func (s *Server) Login(ctx context.Context, in *LoginRequest) (*LoginReply, error) {
+	fmt.Println("Loginrequest: ", in.Username)
+	if in.Username == "gavin" && in.Password == "gavin" {
+		tokenString := CreateToken(in.Username)
+		return &LoginReply{Status: "200", Token: tokenString}, nil
 
-func (c AuthToken) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
-	return map[string]string{
-		"authorization": c.Token,
-	}, nil
-}
-
-func (c AuthToken) RequireTransportSecurity() bool {
-	return false
-}
-
-// Claims defines the struct containing the token claims.
-type Claims struct {
-	jwt.StandardClaims
-
-	// Username defines the identity of the user.
-	Username string `json:"username"`
-}
-
-// Step1. 从 context 的 metadata 中，取出 token
-
-func getTokenFromContext(ctx context.Context) (string, error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return "", fmt.Errorf("ErrNoMetadataInContext")
-	}
-	// md 的类型是 type MD map[string][]string
-	token, ok := md["authorization"]
-	if !ok || len(token) == 0 {
-		return "", fmt.Errorf("ErrNoAuthorizationInMetadata")
-	}
-	// 因此，token 是一个字符串数组，我们只用了 token[0]
-	return token[0], nil
-}
-
-func CheckAuth(ctx context.Context) (username string) {
-	tokenStr, err := getTokenFromContext(ctx)
-	if err != nil {
-		panic("get token from context error")
-	}
-	var clientClaims Claims
-	token, err := jwt.ParseWithClaims(tokenStr, &clientClaims, func(token *jwt.Token) (interface{}, error) {
-		if token.Header["alg"] != "HS256" {
-			panic("ErrInvalidAlgorithm")
-		}
-		return []byte("verysecret"), nil
-	})
-	if err != nil {
-		panic("jwt parse error")
+	} else {
+		return &LoginReply{Status: "403", Token: ""}, nil
 	}
 
-	if !token.Valid {
-		panic("ErrInvalidToken")
-	}
-
-	return clientClaims.Username
 }
 
+// SayHello generates response to a Ping request
+func (s *Server) SayHello(ctx context.Context, in *PingMessage) (*PingMessage, error) {
+	msg := "bar"
+	userName := CheckAuth(ctx)
+	msg += " " + userName
+	return &PingMessage{Greeting: msg}, nil
+}
