a0b0167c868dea99a2f0d66ab46914a01cdbcd36
shitou <shitou@bolo.em>
2022-03-07 11:00:39 +0800

    test code

--- test/reflect.go
+++ test/reflect.go
@@ -24,5 +24,20 @@ func main(){
         }
     }
     fmt.Println(person3)
+    reflect2()
+}
+
+func reflect2(){
+    var x float64 = 5.3
+    fmt.Println("type:", reflect.TypeOf(x))
+    v := reflect.ValueOf(x)
+    fmt.Println("value:", v)
+    fmt.Println("type:", v.Type())
+    fmt.Println("kind:", v.Kind())
+    fmt.Println("value:", v.Float())
+    fmt.Println(v.Interface())
+    fmt.Printf("value is %5.2e\n", v.Interface())
+    y := v.Interface().(float64)
+    fmt.Println(y)
 }
 
