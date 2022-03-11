7ec32067ed36ef35487318e7bc47fa23f4aaa78e
shitou <shitou@bolo.em>
2022-03-11 19:00:39 +0800

    test code

--- test/timer.go
+++ test/timer.go
@@ -16,7 +16,7 @@ func parse1(){
     timestamp := 1602482956
     fmt.Println(reflect.TypeOf(timestamp))
     timeobj := time.Unix(int64(timestamp), 0)
-    date := timeobj.Format("2006-01-02 15:04:05")
+    date := timeobj.Format("2006-01-02 15:04:05")//06-01-02 15:04:05, 2006/01/02 15:04:05, 2006/01/02, 2006-01-02
     fmt.Println(date)
 }
 func parse2(){
@@ -27,5 +27,11 @@ func parse2(){
 }
 func parse3(){
     fmt.Println(time.Millisecond)
+    fmt.Println(time.Now())
+    t := time.Now().Unix()
+    fmt.Println(reflect.TypeOf(t))
+    fmt.Println(time.Unix(t, 0).Format("2006-01-02 15:04:05"))
+    t += 86400 //增加一天
+    fmt.Println(time.Unix(t, 0).Format("2006-01-02 15:04:05"))
 }
 
