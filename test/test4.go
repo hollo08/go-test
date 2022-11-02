1b864e38d2ddf88b8674d9ef9ae061ed60506dc5
shitou <shitou@bolo.em>
2022-11-02 18:54:16 +0800

    test5

--- test/test4.go
+++ test/test4.go
@@ -9,8 +9,7 @@ type Circle struct {
 	radius float64
 }
 
-
-func main()  {
+func main() {
 	var c Circle
 	fmt.Println(c.radius)
 	c.radius = 10.00
@@ -19,12 +18,14 @@ func main()  {
 	fmt.Println(c.radius)
 	change(&c, 30)
 	fmt.Println(c.radius)
+	fmt.Println("a5")
 }
-func (c Circle) getArea() float64  {
+func (c Circle) getArea() float64 {
 	return c.radius * c.radius
 }
+
 // 注意如果想要更改成功c的值，这里需要传指针
-func (c *Circle) changeRadius(radius float64)  {
+func (c *Circle) changeRadius(radius float64) {
 	c.radius = radius
 }
 
@@ -33,6 +34,6 @@ func (c *Circle) changeRadius(radius float64)  {
 //   c.radius = radius
 //}
 // 引用类型要想改变值需要传指针
-func change(c *Circle, radius float64)  {
+func change(c *Circle, radius float64) {
 	c.radius = radius
 }
