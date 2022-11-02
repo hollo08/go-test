4f77b0e56aa01cb5cc11dfbb366fe48b7bbb95be
shitou <shitou@bolo.em>
2022-11-02 18:50:13 +0800

    test2

--- test/test2.go
+++ test/test2.go
@@ -9,17 +9,18 @@ func main() {
 	var i, j int
 
 	for i = 2; i < 100; i++ {
-		for j=2; j <= (i/j); j++ {
-			if(i%j==0) {
-				fmt.Printf("%d  不是素数\n", i);
-				break; // 如果发现因子，则不是素数
+		for j = 2; j <= (i / j); j++ {
+			if i%j == 0 {
+				fmt.Printf("%d  不是素数\n", i)
+				break // 如果发现因子，则不是素数
 			}
 
-			break;
+			break
 		}
 		//if(j > (i/j)) {
-		fmt.Printf("%d  是素数\n", i);
+		fmt.Printf("%d  是素数\n", i)
 		//}
 	}
 	fmt.Println(strings.ContainsAny("in failure", "s g"))
+	fmt.Println("a2")
 }
